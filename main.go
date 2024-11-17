b61c63c431a915f4037301b1d8a9eb60464d90ec
Aniket Hazra <aniket.hazra@bajajfinserv.in>
2024-11-17 13:11:26 +0530

    Boiler Plate Code

--- main.go
+++ main.go
@@ -1,9 +1,46 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("Hello World")
+
+	godotenv.Load()
+
+	portString := os.Getenv("PORT")
+
+	if portString == "" {
+		log.Fatal("PORT is noy found")
+	}
+
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{"*"},
+	}))
+
+	v1Router := chi.NewRouter()
+	v1Router.Get("/probe", handlerReadiness)
+	v1Router.Get("/err", handleErr)
+
+	router.Mount("/v1", v1Router)
+
+	server := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Printf("Sever starting on PORT %v", portString)
+	err := server.ListenAndServe()
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
